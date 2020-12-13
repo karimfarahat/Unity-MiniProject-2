2ebab8561de8ba78bc52e635b3344f6acf29002c
karimfarahat <47497813+karimfarahat@users.noreply.github.com>
2020-12-14 00:06:17 +0200

    project 2

---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    public GameObject Wheel;
+    //public Animator buttonAnimator;
+    //public Animation buttonAnimation;
+    public float genSpeed;
+    public float subSpeed;
+    public bool isSpinning = false;
+    
+
+    public void OnMouseDown()
+    {
+        GetComponent<Animator>().SetBool("isPressed", true);
+        genSpeed = Random.Range(5.000f, 10.000f);
+        subSpeed = Random.Range(0.003f, 0.009f);
+        isSpinning = true;
+    }
+    private void OnMouseUp()
+    {
+        GetComponent<Animator>().SetBool("isPressed", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpinning == true)
+        {
+            
+            Wheel.transform.Rotate(0, 0, -genSpeed, Space.World);
+            genSpeed -= subSpeed;
+            if (genSpeed <= 0)
+            {
+                isSpinning = false;
+                
+            }
+
+        }
+
+    }
+}
+
+
