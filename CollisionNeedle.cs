02ed98d8795961564adcb82f5978f9fbb190c7b5
karimfarahat <47497813+karimfarahat@users.noreply.github.com>
2020-12-15 20:08:24 +0200

    wheel controller and Collision of needle scripts

---
+++ CollisionNeedle.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionNeedle : MonoBehaviour
+{
+    public WheelController WheelController;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (WheelController.isSpinning == false)
+        {
+
+            if (WheelController.doAction == true)
+            {
+
+                if (other.tag == "yellow")
+                {
+                    Debug.Log("yellow hit");
+                }
+                if (other.tag == "red")
+                {
+                    Debug.Log("red hit");
+                }
+                if (other.tag == "green")
+                {
+                    Debug.Log("green hit");
+                }
+                if (other.tag == "blue")
+                {
+                    Debug.Log("blue hit");
+                }
+                if (other.tag == "pink")
+                {
+                    Debug.Log("pink hit");
+                }
+                if (other.tag == "purple")
+                {
+                    Debug.Log("purple hit");
+                }
+                if (other.tag == "orange")
+                {
+                    Debug.Log("orange hit");
+                }
+            }
+            WheelController.doAction = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ WheelController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    public GameObject Wheel;
+    public GameObject Needle;
+
+    public float genSpeed;
+    public float subSpeed;
+    public bool isSpinning = false;
+    public bool doAction = false;
+    bool move = true;
+
+    public void OnMouseDown()
+    {
+        GetComponent<Animator>().SetBool("isPressed", true);
+        genSpeed = Random.Range(5.000f, 10.000f);
+        subSpeed = Random.Range(0.003f, 0.009f);
+        isSpinning = true;
+    }
+    private void OnMouseUp()
+    {
+        GetComponent<Animator>().SetBool("isPressed", false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //law beyspin
+        if (isSpinning == true)
+        {
+
+            Wheel.transform.Rotate(0, 0, -genSpeed, Space.World);
+            genSpeed -= subSpeed;
+            if (genSpeed <= 0)
+            {
+                isSpinning = false;
+                doAction = true;
+
+            }
+
+        }
+        else
+        {
+
+            //law batal yespin w hanharak el needle lel wheel
+            if (doAction == true && move == true)
+            {
+                Needle.transform.Translate(0, 0, 0.5f, Space.World);
+                move = false;
+
+            }
+
+            //law wa2ef wel needle fel wheel
+            else if (doAction == false && move == false)
+            {
+                Needle.transform.Translate(0, 0, -0.5f, Space.World);
+                move = true;
+
+            }
+
+        }
+
+
+    }
+}
+
+
