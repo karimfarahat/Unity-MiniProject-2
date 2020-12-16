161d75948c66c5212652457339b09c44eb27329c
De3bs <ahmede3bs@gmail.com>
2020-12-16 23:58:03 +0200

    Animation and positioning on panel

--- Project 2/Assets/Scripts/CollisionNeedle.cs
+++ Project 2/Assets/Scripts/CollisionNeedle.cs
@@ -32,6 +32,8 @@ public class CollisionNeedle : MonoBehaviour
     {
 
         panel.SetActive(false);
+       
+
     }
 
     public void openPanel(string title, string desc, GameObject fruit)
@@ -40,8 +42,8 @@ public class CollisionNeedle : MonoBehaviour
             titleText.text = title;
             textText.text =desc;
             buttonText.text =  SpinA;
-            
-        
+        fruit.transform.position = fruitPos.transform.position;
+        fruit.GetComponent<Animator>().SetBool("animate", true);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -58,7 +60,7 @@ public class CollisionNeedle : MonoBehaviour
                     string title = "Guava";
                     string Descrip = "Guava Juice is a quick source of nutrition to refresh you in this sweltering summer heat . Guava Juice keeps you hydrated and is a delicious source of Vitamin C.";
 
-                    openPanel(title, Descrip);
+                    openPanel(title, Descrip, guava );
                     
                 }
                 if (other.tag == "red")
@@ -67,7 +69,7 @@ public class CollisionNeedle : MonoBehaviour
 
                     string title = "Apple";
                     string Descrip = "Apple juice can do more than hydrate. It's also rich in vitamin C — and if you pick a brand that's added an extra vitamin boost to its juice, one serving could help you meet more than 100 percent of your recommended daily intake of vitamin C.";
-                    openPanel(title, Descrip);
+                    openPanel(title, Descrip, apple);
                 }
                 if (other.tag == "green")
                 {
@@ -75,7 +77,7 @@ public class CollisionNeedle : MonoBehaviour
 
                     string title = "Pineapple";
                     string Descrip = "Pineapple health juice benefits include immune booster and inflammation fighter, helps to detoxify the body and mind, incredibly great for vision, regulates blood pressure, cancer protective, strengthens gums, promotes digestion, treats ulcerative colitis, good for arthritic patients, reduces menstrual cramps, supports healthy skin, cures asthma.";
-                    openPanel(title, Descrip);
+                    openPanel(title, Descrip, pineapple);
                 }
                 if (other.tag == "blue")
                 {
@@ -83,7 +85,7 @@ public class CollisionNeedle : MonoBehaviour
 
                     string title = "Milk";
                     string Descrip = "Milk is an excellent source of vitamins and minerals.It provides potassium, B12, calcium and vitamin D, which are lacking in many diets(4Trusted Source).Milk is also a good source of vitamin A, magnesium, zinc and thiamine(B1).Additionally, it’s an excellent source of protein and contains hundreds of different fatty acids, including conjugated linoleic acid(CLA) and omega-3s(5Trusted Source).";
-                    openPanel(title, Descrip);
+                    openPanel(title, Descrip, milk);
                 }
                 if (other.tag == "pink")
                 {
@@ -91,7 +93,7 @@ public class CollisionNeedle : MonoBehaviour
 
                     string title = "Mango";
                     string Descrip = "It’s a drupe, or stone fruit, which means that it has a large seed in the middle.This fruit is not only delicious but also boasts an impressive nutritional profile.In fact, studies link mango and its nutrients to health benefits, such as improved immunity, digestive health and eyesight, as well as a lower risk of certain cancers.";
-                    openPanel(title, Descrip);
+                    openPanel(title, Descrip, mango);
                 }
                 if (other.tag == "purple")
                 {
@@ -99,7 +101,7 @@ public class CollisionNeedle : MonoBehaviour
 
                     string title = "Black Berries";
                     string Descrip = "Blackberries offer many health benefits, including: Full of vitamins and minerals like C, K, and manganese. High in fiber. May boost brain health.";
-                    openPanel(title, Descrip);
+                    openPanel(title, Descrip, blackberry);
                 }
                 if (other.tag == "orange")
                 {
@@ -107,10 +109,17 @@ public class CollisionNeedle : MonoBehaviour
 
                     string title = "Orange";
                     string Descrip = "Orange juice is enjoyed around the world.It’s naturally high in vital nutrients, such as vitamin C and potassium.Plus, commercial varieties are often enriched with calcium and vitamin D.";
-                    openPanel(title, Descrip);
+                    openPanel(title, Descrip, orange);
                 }
             }
             WheelController.doAction = false;
+/*            guava.GetComponent<Animator>().SetBool("animate", false);
+            orange.GetComponent<Animator>().SetBool("animate", false);
+            apple.GetComponent<Animator>().SetBool("animate", false);
+            milk.GetComponent<Animator>().SetBool("animate", false);
+            blackberry.GetComponent<Animator>().SetBool("animate", false);
+            mango.GetComponent<Animator>().SetBool("animate", false);
+            pineapple.GetComponent<Animator>().SetBool("animate", false);*/
         }
     }
     // Update is called once per frame
